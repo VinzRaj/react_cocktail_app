050b9a928f5a144a769c05ae7b8d70f330b105b6
VinzRaj <vinny44@gmail.com>
2022-01-20 19:17:33 -0600

    Initial Router set up is done

--- src/App.js
+++ src/App.js
@@ -1,8 +1,29 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+
+import Home from './Pages/Home';
+import About from './Pages/About';
+import Error from './Pages/Error';
+import CocktailDetailPage from './Pages/CocktailDetailPage';
+
 function App() {
   return (
-    <div>
-      <h2>app component</h2>
-    </div>
+    <Router>
+      <Switch>
+        <Route exact path='/'>
+          <Home />
+        </Route>
+        <Route exact path='/about'>
+          <About />
+        </Route>
+        <Route exact path='/cocktail/:id'>
+          <CocktailDetailPage />
+        </Route>
+        <Route path='*'>
+          <Error />
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
