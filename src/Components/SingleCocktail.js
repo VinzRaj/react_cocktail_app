c20bc45157e10e587e2810dba83044ff1eb16e29
VinzRaj <vinny44@gmail.com>
2022-01-21 08:58:41 -0600

    CSS items are disorderd issue solved by setting width and minor fixes details to be displayed

--- src/Components/SingleCocktail.js
+++ src/Components/SingleCocktail.js
@@ -7,7 +7,7 @@ export const SingleCocktail = ({ id, name, image, category, info, glass }) => {
       <div className='item-info'>
         <header>
           <h4>{name}</h4>
-          <h4 className='Category'>${category}</h4>
+          <h4>{category}</h4>
         </header>
 
         <p className='item-text'>{info}</p>
