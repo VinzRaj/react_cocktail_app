3ece99a83897807fb50c8320e02768e3a261d706
VinzRaj <vinny44@gmail.com>
2022-01-20 18:37:34 -0600

    Components created

---
+++ src/Components/CocktailList.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const CocktailList = () => {
+  return (
+    <div>
+      <h2>CocktailList</h2>
+    </div>
+  );
+};
+export default CocktailList;
---
+++ src/Components/SingleCocktail.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const SingleCocktail = () => {
+  return (
+    <div>
+      <h2> SingleCocktail component</h2>
+    </div>
+  );
+};
+export default SingleCocktail;
