79b295eb6b5d0f3565f85d9f505c33adb7dc4be4
VinzRaj <vinny44@gmail.com>
2022-01-21 14:10:16 -0600

    Made cocktailDetail page more prettier by adding some css

--- src/Pages/Cocktail.js
+++ src/Pages/Cocktail.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { useState } from 'react';
-import { useParams } from 'react-router-dom';
+import { useParams, Link } from 'react-router-dom';
+import { Loading } from '../Components/Loading';
 
 const Cocktail = () => {
   const { id } = useParams();
@@ -56,9 +57,51 @@ const Cocktail = () => {
       setLoading(false);
     }
     getCocktail();
-  }, []);
+  }, [id]);
 
-  return <div>hai</div>;
+  if (loading) {
+    return <Loading />;
+  }
+  if (!cocktail) {
+    return <h2 className='section-title'>no cocktail to display</h2>;
+  } else {
+    const { name, image, category, info, glass, instructions, ingredients } =
+      cocktail;
+    return (
+      <section className='section cocktail-section'>
+        <Link to='/' className='btn btn-details'>
+          back home
+        </Link>
+        <h2 className='section-title'>{name}</h2>
+        <div className='drink'>
+          <img src={image} alt={name}></img>
+          <div className='drink-info'>
+            <p>
+              <span className='drink-data'>name :</span> {name}
+            </p>
+            <p>
+              <span className='drink-data'>category :</span> {category}
+            </p>
+            <p>
+              <span className='drink-data'>info :</span> {info}
+            </p>
+            <p>
+              <span className='drink-data'>glass :</span> {glass}
+            </p>
+            <p>
+              <span className='drink-data'>instructons :</span> {instructions}
+            </p>
+            <p>
+              <span className='drink-data'>ingredients :</span>
+              {ingredients.map((item, index) => {
+                return item ? <span key={index}> {item}</span> : null;
+              })}
+            </p>
+          </div>
+        </div>
+      </section>
+    );
+  }
 };
 
 export default Cocktail;
